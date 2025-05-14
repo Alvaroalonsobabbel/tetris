2bd6473084b2ee45b5bc5b0fd051b302299bcd00
Alvaroalonsobabbel <aalonso@babbel.com>
2025-05-14 19:48:37 +0200

    Add version and debug flags with command line parsing

--- main.go
+++ main.go
@@ -1,25 +1,35 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io"
 	"log"
 	"log/slog"
 	"os"
+	"path/filepath"
 	"tetris/terminal"
 	"tetris/tetris"
 
 	"github.com/eiannone/keyboard"
 )
 
+const VERSION = "v0.0.1"
+
 const (
 	hideCursor = "\033[2J\033[?25l" // also clear screen
 	showCursor = "\n\033[22;0H\n\033[?25h"
+	logFile    = ".tetrisLog"
+
+	// Option Flags
+	debugFlag   = "debug"
+	versionFlag = "version"
 )
 
 var debug bool
 
 func main() {
+	evalOptions()
 	l := initLogger()
 	defer func() {
 		if r := recover(); r != nil {
@@ -33,20 +43,36 @@ func main() {
 	defer fmt.Print(showCursor)
 	tetris := tetris.NewGame()
 	terminal.New(tetris, os.Stdout, l).Start()
-
 }
 
 func initLogger() *slog.Logger {
-	file, err := os.OpenFile("log.txt", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
-	if err != nil {
-		log.Fatalf("unable to open log file: %v", err)
-	}
-
 	out := io.Discard
 	if debug {
-		out = file
-	}
+		homeDir, err := os.UserHomeDir()
+		if err != nil {
+			log.Fatalf("error getting home directory: %v", err)
+		}
 
+		out, err = os.OpenFile(filepath.Join(homeDir, logFile), os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+		if err != nil {
+			log.Fatalf("unable to open log file: %v", err)
+		}
+	}
 	handler := slog.NewJSONHandler(out, &slog.HandlerOptions{Level: slog.LevelDebug})
 	return slog.New(handler)
+}
+
+func evalOptions() {
+	flag.BoolFunc(versionFlag, "Prints version", version)
+	flag.Bool(debugFlag, debug, "Enables debugging into ~/.tetrisLog")
+	if err := flag.CommandLine.Parse(os.Args[1:]); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func version(string) error {
+	fmt.Println(VERSION)
+	os.Exit(0)
+
+	return nil
 }
