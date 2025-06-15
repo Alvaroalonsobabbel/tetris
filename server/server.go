76bbff0921ede1682108f55bd485a6b27c83cb9e
Alvaroalonsobabbel <aalonso@babbel.com>
2025-06-15 09:29:54 +0200

    Include player name in connection log message

--- server/server.go
+++ server/server.go
@@ -130,7 +130,7 @@ func (t *tetrisServer) GameSession(stream grpc.BidiStreamingServer[proto.GameMes
 				}
 			}()
 
-			log.Printf("Player %d connected to game: %s", rcv.Player, rcv.GetGameId())
+			log.Printf("%s (player %d), connected to game: %s", rcv.Name, rcv.Player, rcv.GetGameId())
 
 			switch rcv.Player {
 			case player1:
